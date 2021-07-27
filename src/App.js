316cda8b900dc9f74b44b5c4ff8db2d6788b1b87
Lucian Boatright <lucian.boatright@gmail.com>
2021-07-27 19:41:19 +0100

    modal set up and opennig

--- src/App.js
+++ src/App.js
@@ -3,9 +3,42 @@ import './App.css';
 import Watch from './components/Watch'
 import titleImage from './images/watchrollfullopen.webp';
 import { db } from './firebase/firebase.js';
+import { Button, makeStyles } from '@material-ui/core';
+import Modal from '@material-ui/core/Modal';
+
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
   const [watch, setWatches] = useState([]);
+  const [open, setOpen] = useState(false)
 
   useEffect(() => {
     db.collection('watches').onSnapshot(snapshot => {
@@ -17,11 +50,21 @@ function App() {
 
   },[]);
 
+  
+
   return (
     <div className="app">
       <div className="app_Header">
         <img className="app_Header_image" id src={titleImage} alt="" style={{width: "120px"}} />
-
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <div style={modalStyle} className={classes.paper}>
+            <h2>I am the modal</h2>
+          </div>
+        </Modal>
+        <Button onClick={() => setOpen(true)}>Sign up</Button>
       </div>
       <h1>POSTS</h1>
       <div className="post__flexbox">
